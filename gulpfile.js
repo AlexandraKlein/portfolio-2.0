7d4d4adfd594c7d5d96484dd13958f1f1d09e13a
aliklein <ali@10etechnology.com>
2020-04-26 15:13:51 -0700

    untrack dist, img renamed assets

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ const shared = {
     return del(["dist"]);
   },
   assets: () => {
-    return gulp.src("src/img/**/*").pipe(gulp.dest("dist/img"));
+    return gulp.src("src/assets/**/*").pipe(gulp.dest("dist/assets"));
   },
   help: (done) => {
     console.info(
