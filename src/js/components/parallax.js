e99d0aa3a3669cd295f4449d2549e4770b88b173
aliklein <ali@10etechnology.com>
2020-04-26 15:52:50 -0700

    fixes

--- src/js/components/parallax.js
+++ src/js/components/parallax.js
@@ -1,10 +1,10 @@
-const TweenLite = require('gsap');
+import { TweenLite, Expo } from "gsap";
 
 export function Parallax() {
-  const $container = $('.active .devices');
-  const $desktop = $('img.desktop');
-  const $laptop = $('img.laptop');
-  const $mobile = $('img.mobile');
+  const $container = $(".active .devices");
+  const $desktop = $("img.desktop");
+  const $laptop = $("img.laptop");
+  const $mobile = $("img.mobile");
   let container_w = $container.width();
   let container_h = $container.height();
 
@@ -13,7 +13,7 @@ export function Parallax() {
     container_h = $container.height();
   });
 
-  $(window).on('mousemove', event => {
+  $(window).on("mousemove", (event) => {
     const pos_x = event.pageX;
     const pos_y = event.pageY;
     const left = container_w / 2 - pos_x;
@@ -22,17 +22,19 @@ export function Parallax() {
     function translateTween(args) {
       TweenLite.to(args.el, 1, {
         css: {
-          transform: `translateX(${left / args.posLeft}px) translateY(${top / args.posTop}px)`
+          transform: `translateX(${left / args.posLeft}px) translateY(${
+            top / args.posTop
+          }px)`,
         },
         ease: Expo.easeOut,
-        overwrite: 'all'
+        overwrite: "all",
       });
     }
 
     const parallaxPropsArray = [
-      { el: $desktop, posLeft: 75, posTop: 35},
-      { el: $laptop, posLeft: 45, posTop: 15},
-      { el: $mobile, posLeft: 55, posTop: 25}
+      { el: $desktop, posLeft: 75, posTop: 35 },
+      { el: $laptop, posLeft: 45, posTop: 15 },
+      { el: $mobile, posLeft: 55, posTop: 25 },
     ];
 
     for (let i = 0; i < parallaxPropsArray.length; i++) {
